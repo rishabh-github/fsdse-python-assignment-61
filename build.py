f9dcb030497d772360b09ea1127aa412fd4eb1b2
rishabh-github <rishabh-github@users.noreply.github.com>
2017-06-22 03:59:24 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,17 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    dic = {}
+    even = 0
+    odd = 0
+
+    for num in list_of_nums:
+        if num%2 == 0:
+            even = even + 1
+        else:
+            odd = odd + 1
+
+    dic.update({'EVEN':even})
+    dic.update({'ODD':odd})
+
+    return dic
+
+print solution([1, 2, 3, 5, 8, 9])
